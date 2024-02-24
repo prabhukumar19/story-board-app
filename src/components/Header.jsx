ab120eb93ec9fec95a44ac5e1db7c8d0022485a5
Prabhu Kumar <prabhukumar636@gmail.com>
2024-02-24 19:55:18 +0530

    Header Component is done and data.json file added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import elipsis from "../assets/icon-vertical-ellipsis.svg";
 import logo from "../assets/logo/svg/logo-black.svg";
+import iconDown from '../assets/icon-chevron-down.svg';
+import iconUp from '../assets/icon-chevron-up.svg';
+
 const Header = () => {
+  const [openDropdown, setOpenDropdown] = useState(false);
+  const onDropdownClick=()=>{
+
+  }
   return (
     <div className=" p-4 fixed left-0 bg-white dark:bg-[#2b2c37] z-50 right-0 ">
       <header className=" flex justify-between dark:text-white items-center  ">
@@ -13,13 +20,13 @@ const Header = () => {
           </h3>
           <div className=" flex items-center ">
             <h3 className=" truncate max-w-[200px] md:text-2xl text-xl font-bold md:ml-20 font-sans  ">
-            
+            Board Name
             </h3>
             <img
-              src={''}
+              src={openDropdown?iconUp:iconDown}
               alt=" dropdown icon"
-              className=" w-3 ml-2 md:hidden"
-              // onClick={onDropdownClick}
+              className=" w-3 ml-2 cursor-pointer md:hidden"
+              onClick={()=>setOpenDropdown(state=>!state)}
             />
           </div>
         </div>
