59e4b677195afa6c8e279771337554f37a4e2a06
Prabhu Kumar <prabhukumar636@gmail.com>
2024-02-25 15:37:02 +0530

    Project completed

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -50,7 +50,7 @@ function Column({ colIndex }) {
     <div
       onDrop={handleOnDrop}
       onDragOver={handleOnDragOver}
-      className="scrollbar-hide   mx-5 pt-[90px] min-w-[280px] "
+      className="scrollbar-hide   mx-2 pt-[90px] min-w-[280px] "
     >
       <p className=" font-semibold flex  items-center  gap-2 tracking-widest md:tracking-[.2em] text-[#828fa3]">
         <div className={`rounded-full w-4 h-4 ${color} `} />
