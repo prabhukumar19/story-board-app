c624833b5a4dfe3b0a352b76483786e4cc0d6863
Prabhu Kumar <prabhukumar636@gmail.com>
2024-02-25 15:42:31 +0530

    scrollbar added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -35,8 +35,8 @@ function Home() {
     <div
       className={
         windowSize[0] >= 768 && isSideBarOpen
-          ? " bg-[#f4f7fd]  scrollbar-hide h-screen flex dark:bg-[#20212c]  overflow-hidden gap-6  ml-[261px]"
-          : "bg-[#f4f7fd]  scrollbar-hide h-screen flex   dark:bg-[#20212c] overflow-hidden gap-6 "
+          ? " bg-[#f4f7fd]  scrollbar-hide h-screen flex dark:bg-[#20212c]  overflow-x-scroll gap-6  ml-[261px]"
+          : "bg-[#f4f7fd]  scrollbar-hide h-screen flex   dark:bg-[#20212c] overflow-x-scroll gap-6 "
       }
     >
       {windowSize[0] >= 768 && (
